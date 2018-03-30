4ddb6cfe169cf22b0be6ecd6464533e35132e3e6
Lisa Nicvert <lisa.nicvert@insa-lyon.fr>
2018-03-30 08:30:21 +0200

    Created destructor

--- String.cpp
+++ String.cpp
@@ -1,3 +1,6 @@
 # include "String.h"
 int String::MAX_LEN_;
 
+String::~String(){
+	// Add the delete [var] here when you make a new manual allocation in a function.
+}
--- String.h
+++ String.h
@@ -8,6 +8,7 @@ class String{
 		char word_[1]; // Effective storage space allocated to the \0 terminated sequence
 		int len_; // The size of the string sequence terminated by \0
 		int capacity_; // The size of the string sequence allocated by user (>= len)
+		~String(); // destructor
 	public:
 };
 // friend operator+(String,String);
