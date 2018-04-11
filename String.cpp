8aa184cba819c3b407c5c04c6392dc4b858e7545
Emi <emilie.mathian@insa-lyon.fr>
2018-04-11 13:53:47 +0200

    Change position of operetor +

--- String.cpp
+++ String.cpp
@@ -100,5 +100,33 @@ String& String::operator=(char c)
 	return *this;
 }
 
+String operator+(const String& s, const char* c )
+	   {
+	
+		String s2(c);
+		std::cout << s2.word_<<std::endl;	
+		int len2 = s2.len_;
+		int len1 = s.len_;
+		int newlenght = len1 + len2;
+		char *newword = new char [newlenght + 1];
+		for (int i=0 ; i<=len1 ; ++i)
+		{	
+			
+			newword[i] = s.word_[i];	
+		}
+		for (int i=0 ; i<=len2 ; ++i)
+		{	
+
+			newword[len1+i] = s2.word_[i];	
+		}
+
+		newword [len1 + len2 + 1 ] = '\0';
+		String s3;
+		s3.word_ = newword;
+		s3.len_ = len1 + len2 ;
+		s3.capacity_ = len1 + len2 + 1;
+		
+		return s3;
+		}
 
 
